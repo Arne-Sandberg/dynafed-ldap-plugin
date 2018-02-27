3ddeac8969909ec46d598bf618319930dc6f9f75
Louise Davies <louise.davies@stfc.ac.uk>
2018-02-27 13:01:53 +0000

    Change test bucket setup

--- auth/setup_test_buckets.py
+++ auth/setup_test_buckets.py
@@ -12,11 +12,11 @@ s3_client = session.client(service_name="s3", endpoint_url="https://ceph-res-gw1
 
 smudge = "/media/sf_Shared/Smudge.jpg"
 other_smudge = "/media/sf_Shared/other_smudge.jpg"
+not_a_cat = "/media/sf_Shared/not_a_cat.jpg"
 
-s3_client.delete_object(Bucket="unprotected-test-bucket", Key="Smudge.jpg")
+s3_client.delete_object(Bucket="unprotected-test-bucket", Key="not_a_cat.jpg")
 s3_client.delete_object(Bucket="authorised-test-bucket", Key="Smudge.jpg")
 s3_client.delete_object(Bucket="unauthorised-test-bucket", Key="Smudge.jpg")
-s3_client.delete_object(Bucket="unprotected-test-bucket", Key="more_smudge/Other_Smudge.jpg")
 s3_client.delete_object(Bucket="authorised-test-bucket", Key="more_smudge/Other_Smudge.jpg")
 s3_client.delete_object(Bucket="unauthorised-test-bucket", Key="more_smudge/Other_Smudge.jpg")
 s3_client.delete_bucket(Bucket="unprotected-test-bucket")
@@ -27,8 +27,7 @@ s3_client.create_bucket(Bucket="unprotected-test-bucket", ACL="public-read-write
 s3_client.create_bucket(Bucket="authorised-test-bucket", ACL="public-read-write")
 s3_client.create_bucket(Bucket="unauthorised-test-bucket", ACL="public-read-write")
 
-s3_client.upload_file(smudge, "unprotected-test-bucket", "Smudge.jpg", ExtraArgs={"ACL": "public-read-write"})
-s3_client.upload_file(other_smudge, "unprotected-test-bucket", "more_smudge/Other_Smudge.jpg", ExtraArgs={"ACL": "public-read-write"})
+s3_client.upload_file(not_a_cat, "unprotected-test-bucket", "not_a_cat.jpg", ExtraArgs={"ACL": "public-read-write"})
 
 s3_client.upload_file(smudge, "authorised-test-bucket", "Smudge.jpg", ExtraArgs={"ACL": "public-read-write"})
 s3_client.upload_file(other_smudge, "authorised-test-bucket", "more_smudge/Other_Smudge.jpg", ExtraArgs={"ACL": "public-read-write"})
